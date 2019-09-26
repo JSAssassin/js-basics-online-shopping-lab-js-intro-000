dbc0f9d572c8fae699ef89c9ed7da9a6df2b29a2
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-26 20:06:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,10 +62,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(cardNumber) {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
-  } else {
+  if(!cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
+
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  cart = [];
+  return cart;
 
 }
